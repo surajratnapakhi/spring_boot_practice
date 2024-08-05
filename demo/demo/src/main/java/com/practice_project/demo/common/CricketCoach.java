0044e9ab8e66b817be4696b92dab5f5ecf30880a
Suraj Ratnaparkhi <surajratnaparkhi@gmail.com>
2024-08-05 23:07:31 +0530

    Refactoring of code

--- demo/demo/src/main/java/com/practice_project/demo/common/CricketCoach.java
+++ demo/demo/src/main/java/com/practice_project/demo/common/CricketCoach.java
@@ -1,4 +1,4 @@
-package com.practice_project.demo;
+package com.practice_project.demo.common;
 
 import org.springframework.stereotype.Component;
 
--- demo/demo/src/main/java/com/practice_project/demo/rest/DemoController.java
+++ demo/demo/src/main/java/com/practice_project/demo/rest/DemoController.java
@@ -1,5 +1,6 @@
-package com.practice_project.demo;
+package com.practice_project.demo.rest;
 
+import com.practice_project.demo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
