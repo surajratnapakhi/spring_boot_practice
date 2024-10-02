41577cc632af6024d984abcce500e5d868aab16e
Suraj Ratnaparkhi <surajratnaparkhi@gmail.com>
2024-10-02 16:58:29 +0530

    fetching from database
    fetching from database

--- demo/demo/src/main/java/com/practice_project/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/practice_project/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.practice_project.demo;
 
 import com.practice_project.demo.Entity.Student;
 import com.practice_project.demo.dao.StudentDAO;
+import com.practice_project.demo.dao.StudentDAOImpl;
+
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,12 +22,27 @@ public class DemoApplication {
     public CommandLineRunner CommandLineRunner(StudentDAO studentDAO)
     {
         return runner -> {
-            createStudent(studentDAO);
+            //createStudent(studentDAO);
+            //readStudent(studentDAO);
+            readAllStudents(studentDAO);
         };
     }
 
+    private void readAllStudents(StudentDAO studentDAO) {
+        List<Student> studList = studentDAO.findAll();
+        for (Student student : studList) {
+            System.out.println(student);
+        }
+    }
+
+    private void readStudent(StudentDAO studentDAO) {
+        Student tempStudent = studentDAO.findById(2);
+        System.out.println("I found a student: "+tempStudent);
+    }
+
     private void createStudent(StudentDAO studentDAO) {
-        Student tempStudent = new Student("suraj", "ratnaparkhi", "sr@gmail.com");
+        //Student tempStudent = new Student("suraj", "ratnaparkhi", "sr@gmail.com");
+        Student tempStudent = new Student("akash", "deulkar", "ad@gmail.com");
 
         studentDAO.save(tempStudent);
     }
--- demo/demo/src/main/java/com/practice_project/demo/dao/StudentDAO.java
+++ demo/demo/src/main/java/com/practice_project/demo/dao/StudentDAO.java
@@ -1,7 +1,15 @@
 package com.practice_project.demo.dao;
 
+import java.util.List;
+
 import com.practice_project.demo.Entity.Student;
+
+import jakarta.persistence.TypedQuery;
 
 public interface StudentDAO {
     void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List <Student> findAll();
 }
